5d7cc8c91be8661399480e8b6034dc2a90a25882
Matteo <matteo.s@hcl.com>
2018-08-08 14:38:14 +0200

    feat(date-diff): add inWeeks test

--- test/specs/date-diff.spec.js
+++ test/specs/date-diff.spec.js
@@ -30,4 +30,33 @@ describe('date-diff', () => {
 			expect(ret).to.equal(0);
 		});
 	});
+
+	describe('inWeeks', () => {
+		it('should return zero if days are in the same week', () => {
+			const d1 = new Date('December 25 2018');
+			const d2 = new Date('December 26 2018');
+
+			const ret = dateDiff.inWeeks(d1, d2);
+
+			expect(ret).to.equal(0);
+		});
+
+		it('should return positive difference if the date are a week away', () => {
+			const d1 = new Date('December 25 2018');
+			const d2 = new Date('January 1 2019');
+
+			const ret = dateDiff.inWeeks(d1, d2);
+
+			expect(ret).to.equal(1);
+		});
+
+		it('should return positive difference if the date are a week away', () => {
+			const d2 = new Date('December 25 2018');
+			const d1 = new Date('January 1 2019');
+
+			const ret = dateDiff.inWeeks(d1, d2);
+
+			expect(ret).to.equal(-1);
+		});
+	});
 });
