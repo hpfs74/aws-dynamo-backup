9582f99d200ab0e507247aa61ef5479cba93804e
Matteo <matteo.s@hcl.com>
2018-08-08 14:59:45 +0200

    refactor(index): refactor main handler to do backup and maintenance

--- src/index.js
+++ src/index.js
@@ -4,31 +4,23 @@ const dynamoHelper = require('./dynamo-helper');
 exports.handler = async (event, context, callback) => {
 	logger.log('Received event', event);
 
-
 	let promises = [];
 
 	try {
 		let tables = dynamoHelper.listTablesToBackup();
-		promises = tables.map(tables => {
-			dynamoHelper.backupTable(tables.)
+		promises = tables.map(table => {
+			let tableName = dynamoHelper.stripTableNameFromArn(table.ResourceARN);
+			let backupHistoryDay = dynamoHelper.getBackupHistoryDay(table);
+
+			dynamoHelper.backupTable(tableName);
+			dynamoHelper.maintenancePlan(tableName, backupHistoryDay);
 		});
-	}
-	catch(err) {
+	} catch (err) {
 		return callback(err);
 	}
-		.listTables()
-		.then(data => {
-			promises = data['AllTables'].map(table =>
-				dynamoHelper.backupTable(table.ResourceARN);
-				dynamoHelper.maintenancePlan(table.ResourceARN);
-			);
 
-			// fullfill all the promises
-			Promise.all(promises)
-				.then(() => callback())
-				.catch(err => callback(err));
-		})
-		.catch(err => {
-			callback(new Error('Unable to retrieve tables. ' + err.message));
-		});
+	// fullfill all the promises
+	Promise.all(promises)
+		.then(() => callback())
+		.catch(err => callback(err));
 };
