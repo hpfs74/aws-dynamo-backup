e84b888ba3b2291769b8bb31dd687415ba739d3b
Kaloyan Arsov <kaloyan_arsov@epam.com>
2018-08-08 18:12:37 +0300

    Minor optimizations & fixes
    Prep for test

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ exports.handler = async (event, context, callback) => {
 	try {
 		let tables = await dynamoHelper.listTablesToBackup();
 		promises = tables.map(table => {
-			let tableName = dynamoHelper.stripTableNameFromArn(table.ResourceARN);
-			let backupHistoryDay = dynamoHelper.getBackupHistoryDay(table);
+			const tableName = dynamoHelper.stripTableNameFromArn(table.ResourceARN);
+			const backupHistoryDay = dynamoHelper.getBackupHistoryDay(table);
 
-			dynamoHelper.backupTable(tableName);
-			dynamoHelper.maintenancePlan(tableName, backupHistoryDay);
+			return dynamoHelper.backupTable(tableName).then(() => 
+				dynamoHelper.maintenancePlan(tableName, backupHistoryDay)
+			);
 		});
 	} catch (err) {
 		return callback(err);
--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -1,8 +1,16 @@
 describe('Lambda handler', () => {
 	describe('index', () => {
-		it('should collect all available tables');
-		it('should create chain of promises to run backup');
-		it('should call callback with no error if success');
-		it('should call callback with error if any');
+		it('should collect all available tables', () => {
+			
+		});
+		it('should create chain of promises to run backup', () => {
+
+		});
+		it('should call callback with no error if success', () => {
+
+		});
+		it('should call callback with error if any', () => {
+
+		});
 	});
 });
