5b1bec57906162435ad791890f2f8d8bf2581963
Matteo <matteo.s@hcl.com>
2018-08-08 13:27:23 +0200

    feat(lambda): lambda test placeholder

---
+++ test/specs/index.spec.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('Lambda handler', () => {
+	describe('index', () => {
+		it('should collect all available tables');
+		it('should create chain of promises to run backup');
+		it('should call callback with no error if success');
+		it('should call callback with error if any');
+	});
+});
